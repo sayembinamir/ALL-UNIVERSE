1e1175efbc771a49900e639f3ab99e9310da4ee6
sayembinamir] <sayembinamir912@gmail.com>
2023-03-04 18:19:00 +0600

    finish

--- js/index.js
+++ js/index.js
@@ -36,7 +36,11 @@ displayHub = (hubData, dataLimit) =>{
                             <h5>${hubInfo.name}</h5>
                             <p class='mt-3'> <span class ='me-2'><i class="fa-solid fa-calender-days"></i></span> ${hubInfo.published_in}</p>
                         </div>
-                        <button onclick="loadAIData('${hubInfo.id}')" class="border-0 rounded-circle mt-3" data-bs toggle="modal" target-bs-target ="#hubDetailModal" style ='height:24px'><i class= "fa-solid fa-arrow-right"></i></button>
+                        <div>
+                     <button onclick="loadAIData('${hubInfo.id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <i class="fa-solid fa-right-long"></i>
+                    </button>
+                  <div>
                     </div>
                 </div>
             </div>
@@ -71,53 +75,65 @@ const loadAIData = async id =>{
    const data = await res.json();
    displaySingleDataDetails(data.data);
 }
+ 
 const displaySingleDataDetails =info =>{
-   const modalTitle =document.getElementById('hubDetailModel');
-   const dataDetails = document.getElementById('data-details');
-   dataDetails.classList.add('col', 'p-3');
-   dataDetails.innerHTML=`
-   <div>
-        <h6>${info.description}</h6>
-        <div class='d-flex justify-content-between gap-2 mt-4'>
-            <div>
-                <h6 class="text-success">${info?.pricing?.[0]?.price || 'free of cost'} <br><span>${info.pricing?.[0].plan}</span> </h6>
-            </div>
-            <div>
-                <h6 class="text-warning">${info?.pricing?.[1]?.price || 'free of cost'} <br><span>${info.pricing?.[1].plan}</span> </h6>
-            </div>
-            <div>
-                <h6 class="text-danger">${info?.pricing?.[2]?.price || 'free of cost'} <br><span>${info.pricing?.[2].plan}</span> </h6>
-            </div>
-        </div>
-        <div class="d-flex justify-content-between mt-4">
-            <div>
-                <h5>Features</h5>
-                <ul>
-                    <li>${info.features["1"]["feature_name"] }</li>
-                    <li>${info.features["2"]["feature_name"] }</li>
-                    <li>${info.features["3"]["feature_name"] }</li>
-                </ul>
-            </div>
-            <div>
-                <h6>Integrations</h6>
-                <ul>
-                    ${info.integrations.map(integration => `<li>${integration}</li>`).join(' ')}
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="border rounded p-3">
-    <div class=" position-relative">
-    <img src ="${info.image_link?.[0]}" class="card-img-top rounded"alt="...">
-    <p class="bg-danger text-white rounded position-absolute top-0 and-0">${info.accuracy?.['score'] ? info.accuracy['score'] *100+ `%accuracy`: ''}</p>
-    </div>
-    <h6 class=" text-center">${info.input_output_examples?.[0].input} </h6>
-    <p class="text-center">${info.input_output_examples?.[0].output || 'No! Not yet! take a break !!!'}</p>
-    </div>
-   `
+    const modalTitle =document.getElementById('modelPerent');
+    modalTitle.innerHTML = "";
+    const dataDetails = document.createElement('div');
+    dataDetails.innerHTML=`
+    <div class = "d-flex justify-content-center align-items-center" style="width: 100%">
+         <div class = "col-sm-12 col-md-6">
+             <div class=" p-3 rounded-3">
+                 <h4>${info.description}</h4>
+                 <div class = " d-flex justify-content-between gap-1">
+                     <div class = "text-center p-3 bg-light rounded-3">
+                         <h6 class="text-danger">${info?.pricing?.[0]?.price || 'free of cost'} <br><span>${info.pricing?.[0]?.plan || 'Basic'}</span> </h6>
+                     </div>
+                     <div class = "text-center p-3 bg-light rounded-3">
+                         <h6 class="text-danger">${info?.pricing?.[1]?.price || 'free of cost'} <br><span>${info.pricing?.[0]?.plan || 'Basic'}</span> </h6>
+                     </div>
+                     <div class = "text-center p-3 bg-light rounded-3">
+                         <h6 class="text-danger">${info?.pricing?.[2]?.price || 'free of cost'} <br><span>${info.pricing?.[2]?.plan || 'Basic'}</span> </h6>
+                     </div>
+                 </div>
+                 <div class="row">
+                     <div class = "col">
+                         <h3>Features</h3>
+                         <ul class=""fs-6>
+                             <li>${info?.features?.[1]?.feature_name || 'No Features'}</li>
+                             <li>${info?.features?.[2]?.feature_name || 'No Features'}</li>
+                             <li>${info?.features?.[3]?.feature_name || 'No Features'}</li>
+                         </ul>
+                     </div>
+                     <div class="col">
+                         <h3>Integrations</h3>
+                         <ul>
+                             <li>${info?.integrations?.[0]||'No Integrations'}</li>
+                             <li>${info?.integrations?.[2]||'No Integrations'}</li>
+                             <li>${info?.integrations?.[2]||'No Integrations'}</li>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-sm-12 col-md-6 position-relative">
+                 <div class=" rounded p-3">
+                     <div class=" position-relative">
+                         <img src ="${info.image_link?.[0]}" class="card-img-top rounded"alt="..." style="height: 300px;">
+                         <p class="bg-danger text-white rounded py-2 px-3 position-absolute"style="top: 7px; right: 7px">${info.accuracy?.['score'] ? info.accuracy['score'] *100+ `%accuracy`: ''}</p>
+                     </div>
+                     <h6 class="mt-2 text-center">${info.input_output_examples?.[0]?.input || 'No info'} </h6>
+                     <p class="text-center">${info?.input_output_examples?.[0]?.output || 'No! Not yet! take a break !!!'}</p>
+                 </div>
+             </div>
+     </div>
+     
+     `
+     modalTitle.appendChild(dataDetails)
+ 
+ 
+ 
+ }
 
-
-
-}
 
 loadHub(6);
